517f543d826ab968d892c9fc4f9d9157e636455d
Agus <agus@agus.com>
2015-08-23 19:46:46 -0300

    AHora si, viborita

---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+
+	public GameObject foodPrefab;
+
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	void Start () {
+		InvokeRepeating("Spawn", 3, 4);
+	}
+	
+
+	void Spawn() {
+		int x = (int)Random.Range(borderLeft.position.x+10,
+		                          borderRight.position.x-10);
+		
+
+		int y = (int)Random.Range(borderBottom.position.y+10,
+		                          borderTop.position.y-10);
+		
+	
+		Instantiate(foodPrefab,
+		            new Vector2(x, y),
+		            Quaternion.identity);
+	}
+}
