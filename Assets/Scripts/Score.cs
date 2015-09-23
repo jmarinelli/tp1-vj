7b291228e379ab40c0524886177bcdaac93dd9d9
Agus <agus@agus.com>
2015-09-22 21:10:40 -0300

    listo

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public GameController gameController;
+
+	void OnCollisionEnter(Collision col) {
+		gameController.OnScore();
+	}
+}
---
+++ Assets/Scripts/TargetCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetCollider : MonoBehaviour {
+
+	public GameController gameController;
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log ("Win");
+		gameController.OnWin ();
+	}
+}
